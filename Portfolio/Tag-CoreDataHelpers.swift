5f7d0e28f37153fafeb464ad28900004b7c8be38
Zach Whittle <zach@zachwhittle.com>
2023-08-12 16:06:53 -0500

    clean up core data

---
+++ Portfolio/Tag-CoreDataHelpers.swift
@@ -0,0 +1,46 @@
+//
+//  Tag.swift
+//  Portfolio
+//
+//  Created by Zach Whittle on 8/12/23.
+//
+
+import Foundation
+
+extension Tag {
+    var tagID: UUID {
+        id ?? UUID()
+    }
+    
+    var tagName: String {
+        name ?? ""
+    }
+    
+    var tagActiveIssues: [Issue] {
+        let result = issues?.allObjects as? [Issue] ?? []
+        return result.filter { $0.completed == false }
+    }
+    
+    static var example: Tag {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let tag = Tag(context: viewContext)
+        tag.id = UUID()
+        tag.name = "Example Tag"
+        return tag
+    }
+}
+
+extension Tag: Comparable {
+    public static func <(lhs: Tag, rhs: Tag) -> Bool {
+        let left = lhs.tagName.localizedLowercase
+        let right = lhs.tagName.localizedLowercase
+        
+        if left == right {
+            return lhs.tagID.uuidString < rhs.tagID.uuidString
+        } else {
+            return left < right
+        }
+    }
+}
